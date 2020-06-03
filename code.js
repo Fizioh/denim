b103a776da686ff0675d44f664dfe0d2ec184112
Benosmane Yassine <54205753+Fizioh@users.noreply.github.com>
2020-06-03 12:00:09 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,8 @@
+/* fonction qui cherche élément de contenu h1 et stocke son texte comme chaine de variable */
+window.addEventListener("load",function(){
+    this.window.lastScrollY= 0;
+    var parent = document.querySelector("h1");
+    var txt = parent.innerText;
+    parseInt.innerText = "";
+
+})
