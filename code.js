b796a2ddf3ab7bfca5ced7664077df1f5e8e2190
Benosmane Yassine <54205753+Fizioh@users.noreply.github.com>
2020-06-06 20:26:46 +0200

    Add files via upload

--- code.js
+++ code.js
@@ -1,14 +1,15 @@
 /* fonction qui cherche élément de contenu h1 et stocke son texte comme chaine de variable */
 window.addEventListener("load",function(){
-    this.window.lastScrollY= 0;
+    window.lastScrollY= 0;
     var parent = document.querySelector("h1");
     var txt = parent.innerText;
-    parseInt.innerText = "";
+    parent.innerText = "";
 
     /* chaine txt crée en tant qu'enfants individuel de h1 // propriétés de vitesse et de position et opacité aléatoires */
     for(var i=0; i<txt.length; i++){
         var letter = document.createElement("span");
-        letter.speed = Math.floor(Math.random()*400)+2;
+        letter.speed = Math.floor(Math.random()*10)+2;
+        letter.y = Math.floor(Math.random()*400)+2;
         letter.originY = letter.y;
         letter.opacity = 1;
         letter.innerText = txt[i];
@@ -16,7 +17,7 @@ window.addEventListener("load",function(){
         parent.appendChild(letter);
     }
 
-    this.window.addEventListener("scroll",function(){
+    window.addEventListener("scroll",function(){
         let scrollUp = (window.lastScrollY < window.scrollY ? true : false);
         var nodes = document.querySelectorAll("h1 > *");
         if(window.scrollY < window.screen.height*2){
